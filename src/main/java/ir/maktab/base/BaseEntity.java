d2e4c24b65519fedf888cdeb71142ca1c87f67a7
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-07-23 00:24:54 +0330

    BaseEntity is written

--- src/main/java/ir/maktab/base/BaseEntity.java
+++ src/main/java/ir/maktab/base/BaseEntity.java
@@ -1,4 +1,21 @@
 package ir.maktab.base;
 
-public class BaseEntity {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.io.Serializable;
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@MappedSuperclass
+public class BaseEntity <ID extends Serializable>implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ID id;
 }
